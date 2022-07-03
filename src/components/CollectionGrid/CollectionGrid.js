3f442f1b7fd52e50cf9a0222dc4aa9ef73b84abb
C <45455218+crypblizz8@users.noreply.github.com>
2022-07-03 16:02:47 +0200

    init ZDK call
    Remove the test zdk calls later

---
+++ src/components/CollectionGrid/CollectionGrid.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { testZoraCall } from "../../utils/zoraApi";
+// import { toast } from 'react-toastify'
+// import { useAccount, useNetwork } from 'wagmi'
+// import { isZorbOwner } from '../../utils/zoraApi'
+import Image from 'next/image'
+import MintButton from "../MintButton";
+
+const CollectionGrid = ({data}) => {
+	
+
+	console.log(' zoraCollectionData2 data', data.nodes);
+	
+	// const [pendingTx, setPendingTx] = useState(false)
+	// const { activeChain, chains } = useNetwork()
+	// const { data: account } = useAccount()
+
+	// const handleButtonClick = async () => {
+    //     console.log('button press')
+	// }
+
+    
+    //Can hardcode a MINT URL 
+	return (
+		<div className="grid grid-cols-3 gap-6">
+			{data?.nodes.map((e,i) => {
+				return (
+					<div className="h-96 w-96 m-10" key={i}>
+						<Image src={e?.token.image?.url} alt='blitma' width={400} height={400} />
+						<p>{e.token.name}</p>
+						{/* <MintButton/> */}
+					</div>
+				)
+			})}
+			
+		</div>
+	)
+}
+
+export default CollectionGrid
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+      <foote >
+      <a
+        href="https://twitter.com/crypblizz"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+    </foote>
+)  
+}
+
+export default Footer
